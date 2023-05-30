0d597667b93965a5aa78bf500fdd009c2eb7d163
shadowrunner11 <pieroramirez810@gmail.com>
2023-05-30 12:23:03 -0500

    :tada: initial config

--- apps/docs/.storybook/main.js
+++ apps/docs/.storybook/main.js
@@ -14,10 +14,10 @@ module.exports = {
       resolve: {
         alias: [
           {
-            find: "@acme/core",
+            find: "@vers/core",
             replacement: path.resolve(
               __dirname,
-              "../../../packages/acme-core/"
+              "../../../packages/vers-core/"
             ),
           },
         ],
