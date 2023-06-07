7403ec54fb61d56b059a7a5e9263333f749ce194
jaen <jaenfigueroa03042000@gmail.com>
2023-06-07 17:02:00 -0500

    :sparkles: feat: add background image to hero

--- packages/uiv2/src/pages/Productos/Productos.tsx
+++ packages/uiv2/src/pages/Productos/Productos.tsx
@@ -1,3 +1,4 @@
+import { BackGround } from '../../components/BackGround/BackGround'
 import { CardProduct } from '../../components/CardProduct/CardProduct'
 import { Grid } from '../../components/Grid/Grid'
 import { HeroProducts } from '../../components/HeroProducts/HeroProducts'
@@ -7,20 +8,23 @@ import { Subtitle } from '../../components/Sutitle/Subtitle'
 export const Productos = () => {
   return (
     <section className='vers-grow'>
-      <HeroProducts/>
-      <Main>
-        <Subtitle text='Productos TOP'>
-          <Grid>
-            <>
-              <CardProduct/>
-              <CardProduct/>
-              <CardProduct/>
-              <CardProduct/>
-              <CardProduct/>
-            </>
-          </Grid>
-        </Subtitle>
-      </Main>
-    </section>
+      <BackGround fondoImg='https://i.pinimg.com/originals/e1/78/d2/e178d2bbb7c6202ad66fc2a173331b28.jpg' alt='montaña blanca con un lago en frente'>
+        <HeroProducts/>
+      </BackGround>
+        <Main>
+          <Subtitle text='Productos TOP'>
+            <Grid>
+              <>
+                <CardProduct/>
+                <CardProduct/>
+                <CardProduct/>
+                <CardProduct/>
+                <CardProduct/>
+              </>
+            </Grid>
+          </Subtitle>
+        </Main>
+      </section>
+
   )
 }
