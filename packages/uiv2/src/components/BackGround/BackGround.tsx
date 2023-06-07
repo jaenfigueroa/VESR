b5dd199610fdeb0ae4906a3967c364142fafbbba
jaen <jaenfigueroa03042000@gmail.com>
2023-06-07 18:59:50 -0500

    :sparkles: feat: improve the responsiveness of the hero of the page projects

--- packages/uiv2/src/components/BackGround/BackGround.tsx
+++ packages/uiv2/src/components/BackGround/BackGround.tsx
@@ -6,8 +6,8 @@ type Props = {
 
 export const BackGround = ({children, fondoImg, alt}: Props) => {
   return (
-    <div className='vers-bg-gxxreen vers-relative'>
-      <div className='vers-bg-red vers-w-full vers-h-full vers-absolute vers-z-[-1]'>
+    <div className='vers-relative'>
+      <div className='vers-w-full vers-h-full vers-absolute vers-z-[-1]'>
         <img className='vers-w-full vers-h-full vers-absolute vers-object-cover' src={fondoImg} alt={alt} />
       </div>
       {children}
