084245c63c6c63d47a5b521705b80c751037a9d6
jaen <jaenfigueroa03042000@gmail.com>
2023-06-08 23:34:10 -0500

    :sparkles: feat: add embedded facebook page

--- packages/uiv2/src/components/Facebook/Facebook.tsx
+++ packages/uiv2/src/components/Facebook/Facebook.tsx
@@ -1,15 +1,39 @@
+import { useRef, useEffect, useState } from 'react'
+
 export const Facebook = () => {
+  const targetRef = useRef<HTMLIFrameElement>(null)
+  const [width, setWidth] = useState<number>(0)
+  const [height, setHeight] = useState<number>(0)
+
+  useEffect(() => {
+    const target = targetRef.current
+    if (!target) return
+
+    const resizeObserver = new ResizeObserver((entries) => {
+      for (const entry of entries) {
+        const { width, height } = entry.contentRect
+        setWidth(Math.floor(width))
+        setHeight(Math.floor(height))
+      }
+    })
+
+    resizeObserver.observe(target)
+
+    return () => {
+      resizeObserver.unobserve(target)
+    }
+  }, [])
+
   return (
     <div className='vers-w- vers-flex vers-flex-col vers-bg-purplexxxxxxx-100 vers-gap-2'>
       <div className='vers-w-full'>
         <img className='vers-w-full' src='/Frame 8.png' alt='Piezas blancas donde se lee facebook' />
       </div>
-      <div className='vers-w-full vers-h-[400px] md:vers-h-full vers-bg-light-gray vers-relative'>
+      <div className='vers-w-full vers-h-[400px] md:vers-h-full vers-bg-light-gray vers-relative vers-max-w-[500px] vers-m-auto'>
         <iframe
-          className='vers-border-2 vers-borde-solid vers-border-red vers-w-full vers-h-full'
-          src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FVESRoficial&tabs=timeline&width=340&height=500&small_header=true&adapt_container_width=false&hide_cover=false&show_facepile=true&appId"
-          width="200"
-          height="300"
+          ref={targetRef}
+          className='vers-border-2 vers-borde-solid versxxx-border-red vers-w-full vers-h-full vers-border-none'
+          src={`https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FVESRoficial&tabs=timeline&width=${width}&height=${height}&small_header=true&adapt_container_width=false&hide_cover=false&show_facepile=true&appId`}
           allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
       </div>
     </div>
