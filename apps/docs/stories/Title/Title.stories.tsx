a4d12eaec8abde6499ff0c9bce5c12959643d935
jaen <jaenfigueroa03042000@gmail.com>
2023-06-09 17:57:24 -0500

    :recycle: refactor: change content to Title component

--- apps/docs/stories/Title/Title.stories.tsx
+++ apps/docs/stories/Title/Title.stories.tsx
@@ -8,9 +8,11 @@ const meta:Meta<typeof Title> = {
   tags: ['autodocs'],
   argTypes: {
     text: {
+      description: 'Titulo de la seccion',
       control: 'text',
     },
     children: {
+      description: 'Contenido de la seccion',
       control: 'text',
     }
   },
@@ -21,7 +23,7 @@ type Story = StoryObj<typeof Title>
 export const Example1:Story = {
   args: {
     text: 'objetivos',
-    children: <h1 className='vers-p-2 vers-border-dashed vers-border-[1px] vers-color-light-gray'>Aqui va el contenido</h1>
+    children: <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original.</p>
   },
 }
 
