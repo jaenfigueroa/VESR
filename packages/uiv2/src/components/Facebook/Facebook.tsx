234a6bb027da7bae2a809f01daaaee58566b5a7d
jaen <jaenfigueroa03042000@gmail.com>
2023-06-08 22:25:18 -0500

    :sparkles: feat: add iframe to facebook

--- packages/uiv2/src/components/Facebook/Facebook.tsx
+++ packages/uiv2/src/components/Facebook/Facebook.tsx
@@ -4,8 +4,13 @@ export const Facebook = () => {
       <div className='vers-w-full'>
         <img className='vers-w-full' src='/Frame 8.png' alt='Piezas blancas donde se lee facebook' />
       </div>
-      <div className='vers-w-full vers-h-[400px] md:vers-h-full vers-bg-light-gray vers-p-2'>
-        Publicaciones de Facebook
+      <div className='vers-w-full vers-h-[400px] md:vers-h-full vers-bg-light-gray vers-relative'>
+        <iframe
+          className='vers-border-2 vers-borde-solid vers-border-red vers-w-full vers-h-full'
+          src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FVESRoficial&tabs=timeline&width=340&height=500&small_header=true&adapt_container_width=false&hide_cover=false&show_facepile=true&appId"
+          width="200"
+          height="300"
+          allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
       </div>
     </div>
   )
