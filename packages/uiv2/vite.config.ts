9050dcf62b39a26d05b837cb5e7c6e635cd5973b
jaen <jaenfigueroa03042000@gmail.com>
2023-06-13 19:06:16 -0500

    :rocket: build: update release.yml

--- packages/uiv2/vite.config.ts
+++ packages/uiv2/vite.config.ts
@@ -1,26 +1,26 @@
 import path from 'path';
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),dts({insertTypesEntry: true})],
-  build: {
-    lib: {
-        entry: path.resolve(__dirname, 'src/main.tsx'),
-        name: 'uiv2',
-        formats: ['es', 'umd'],
-        fileName: (format) => `uiv2.${format}.js`,
-    },
-    rollupOptions: {
-        external: ['react', 'react-dom' ],
-        output: {
-            globals: {
-                react: 'React',
-                'react-dom': 'ReactDOM',
+    plugins: [react(), dts({ insertTypesEntry: true })],
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, 'src/main.tsx'),
+            name: 'uiv2',
+            formats: ['es', 'umd'],
+            fileName: (format) => `uiv2.${format}.js`,
+        },
+        rollupOptions: {
+            external: ['react', 'react-dom'],
+            output: {
+                globals: {
+                    react: 'React',
+                    'react-dom': 'ReactDOM',
+                },
             },
         },
     },
-}
-})
+});
