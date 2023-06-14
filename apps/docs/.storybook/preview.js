628241e165b55b84bdb9e06866a04917c01be37e
jaen <jaenfigueroa03042000@gmail.com>
2023-06-13 20:45:47 -0500

    :bug: fix: change the css path

--- apps/docs/.storybook/preview.js
+++ apps/docs/.storybook/preview.js
@@ -1,5 +1,6 @@
 // ESTILOS TAILWIND CSS
-import '@vers/uiv2/dist/style.css';
+// import '@vers/uiv2/dist/style.css';
+import '@vers/uiv2/dist/assets/index-38b9129e.css';
 import '@vers/uiv2/src/index.css';
 //ICONOS AWESOME
 import '@fortawesome/fontawesome-svg-core/styles.css'
--- packages/uiv2/vite.config.ts
+++ packages/uiv2/vite.config.ts
@@ -1,26 +1,7 @@
-import path from 'path';
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import dts from 'vite-plugin-dts';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), dts({ insertTypesEntry: true })],
-    build: {
-        lib: {
-            entry: path.resolve(__dirname, 'src/main.tsx'),
-            name: 'uiv2',
-            formats: ['es', 'umd'],
-            fileName: (format) => `uiv2.${format}.js`,
-        },
-        rollupOptions: {
-            external: ['react', 'react-dom'],
-            output: {
-                globals: {
-                    react: 'React',
-                    'react-dom': 'ReactDOM',
-                },
-            },
-        },
-    },
+  plugins: [react()],
 });
